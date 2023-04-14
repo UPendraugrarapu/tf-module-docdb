80f8c33ce25504568c2f287d4511936397d6a4e2
upendra <ugrarapuupendra@gmail.com>
2023-04-14 11:21:42 +0530

    upd

--- vars.tf
+++ vars.tf
@@ -1,2 +1,8 @@
 variable "env" {}
 variable "engine" {}
+variable "backup_retention_period" {}
+varaiable "preferred_backup_window" {}
+varaiable "skip_final_snapshot" {}
+varaiable "engine_version" {}
+varaiable "subnet_ids" {}
+varaiable "tags" {}
